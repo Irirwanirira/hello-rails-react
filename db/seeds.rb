cfedafaef92bb96e5b16271804d9aacf028d16d8
Irirwanirira <irijoseph41@gmail.com>
2023-03-02 11:31:03 +0200

    Add greeting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create([
+    { message: 'We are so close to the program' },
+    { message: 'Sleepless night' },
+    { message: 'Day before capstone' },
+    { message: 'Magical sun set' },
+    { message: 'Where was Auto magical ChatGpt' },
+    { message: 'Ask sister ChatGpt' }
+  ])
