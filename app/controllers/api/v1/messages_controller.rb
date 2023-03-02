67a2c03a1322f72e836c284ff2b7d813b458f1ce
Irirwanirira <irijoseph41@gmail.com>
2023-03-02 12:12:28 +0200

    Set linters

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,11 @@
 module Api
-    module V1
-      # my comment
-      class MessagesController < ApplicationController
-        def index
-          @greetings = Message.all.sample
-          render json: @greetings
-        end
+  module V1
+    # my comment
+    class MessagesController < ApplicationController
+      def index
+        @greetings = Message.all.sample
+        render json: @greetings
       end
     end
   end
+end
